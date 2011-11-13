e31d01fc7564ebbd71d82c35e4441bdf8bfb1b19
Dave Vasilevsky <dave@vasilevsky.ca>
2011-11-12 19:21:55 -0500

    Read bloc, decompress it, read base inode

--- squashfuse.c
+++ squashfuse.c
@@ -1,6 +1,11 @@
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
+
+#include <zlib.h>
 
 #include "squashfs_fs.h"
 #include "swap.h"
@@ -27,12 +32,72 @@ int main(int argc, char *argv[]) {
 	sqfs_swapin_super_block(&sb);
 	if (sb.s_magic != SQUASHFS_MAGIC) {
 		fprintf(stderr, "Not a squashfs image\n");
+		// TODO: Check version, compression
+		goto cleanup;
+	}	
+	printf("Inodes: %d\n", sb.inodes);
+	
+	uint32_t root_block = (sb.root_inode >> 16) + sb.inode_table_start;
+	uint16_t root_offset = sb.root_inode & 0xffff;
+	
+	off_t pos = root_block;
+	uint16_t hdr;
+	if (pread(fd, &hdr, sizeof(hdr), pos) != sizeof(hdr)) {
+		perror("Reading block header");
 		goto cleanup;
 	}
+	pos += sizeof(hdr);
+	hdr = sqfs_swapin16(hdr);
+	bool compressed = !(hdr & SQUASHFS_COMPRESSED_BIT);
+	size_t len = hdr & ~SQUASHFS_COMPRESSED_BIT;
+	if (!len)
+		len = SQUASHFS_COMPRESSED_BIT;
 	
-	printf("Inodes: %d\n", sb.inodes);
+	char *block = malloc(len);
+	if (!block) {
+		fprintf(stderr, "Failed to allocate block data\n");
+		goto cleanup;
+	}
+	if (pread(fd, block, len, pos) != len) {
+		perror("Reading block");
+		goto cleanup_block;
+	}
+	
+	size_t olen = len;
+	if (compressed) {
+		size_t decomp_size = SQUASHFS_METADATA_SIZE;
+		char *decomp = malloc(decomp_size);
+		if (!decomp) {
+			fprintf(stderr, "Failed to allocate decompression output\n");
+			goto cleanup_block;
+		}
+		int zerr = uncompress((Bytef*)decomp, &decomp_size,
+			(Bytef*)block, len);
+		if (zerr != Z_OK) {
+			fprintf(stderr, "zlib error %d\n", zerr);
+			free(decomp);
+			goto cleanup_block;
+		}
+		free(block);
+		block = decomp;
+		olen = decomp_size;
+	}
+	
+	struct squashfs_base_inode *base;
+	if (root_offset + sizeof(*base) > olen) {
+		fprintf(stderr, "FIXME: Need multiple blocks for inode\n");
+		err = 0;
+		goto cleanup_block;
+	}
+	base = (struct squashfs_base_inode*)(block + root_offset);
+	sqfs_swapin_base_inode(base);
+	
+	time_t mtime = base->mtime;
+	printf("Root mtime: %s", ctime(&mtime));
+	
 	err = 0;
-	
+cleanup_block:
+	free(block);
 cleanup:
 	close(fd);
 	return err;
