1a0af66511e8a5d4a47e87083c34e0200fa14b0f
Dave Vasilevsky <dave@vasilevsky.ca>
2014-05-30 15:55:04 -0400

    Promote 'squashfuse_hl' to 'squashfuse'
    The low-level driver now becomes 'squashfuse_ll'. This means users can
    use the same name as they're used to, whether or not they have the
    low-level API. The speed difference is not high enough to matter that much.

--- ll.c
+++ ll.c
@@ -23,320 +23,443 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "config.h"
-#include "ll.h"
-#include "hash.h"
-#include "nonstd.h"
 
 #include <errno.h>
+#include <fcntl.h>
+#include <float.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
+#include "ll.h"
+#include "squashfuse.h"
+#include "nonstd.h"
+#include "fuseprivate.h"
 
-/***** INODE CONVERSION FOR 64-BIT INODES ****
- *
- * sqfs(root) maps to FUSE_ROOT_ID == 1
- * sqfs(0) maps to 2
- *
- * Both 1 and 2 are guaranteed not to be used by sqfs, due to inode size
- */
-static fuse_ino_t sqfs_ll_ino64_fuse(sqfs_ll *ll, sqfs_inode_id i) {
-	if (i == ll->fs.sb.root_inode) {
-		return FUSE_ROOT_ID;
-	} else if (i == 0) {
-		return 2;
-	} else {
-		return i;
-	}
-}
+static const double SQFS_TIMEOUT = DBL_MAX;
 
-static sqfs_inode_id sqfs_ll_ino64_sqfs(sqfs_ll *ll, fuse_ino_t i) {
-	if (i == FUSE_ROOT_ID) {
-		return ll->fs.sb.root_inode;
-	} else if (i == 2) {
-		return 0;
-	} else {
-		return i;
-	}
-}
-
-static fuse_ino_t sqfs_ll_ino64_fuse_num(sqfs_ll *ll, sqfs_dir_entry *e) {
-	return sqfs_ll_ino64_fuse(ll, sqfs_dentry_inode(e));
-}
-
-static sqfs_err sqfs_ll_ino64_init(sqfs_ll *ll) {
-	ll->ino_fuse = sqfs_ll_ino64_fuse;
-	ll->ino_sqfs = sqfs_ll_ino64_sqfs;
-	ll->ino_fuse_num = sqfs_ll_ino64_fuse_num;
-	return SQFS_OK;
-}
-
-
-
-/***** INODE CONVERSION FOR 32-BIT INODES ****
- *
- * We maintain a cache of sqfs_inode_num => sqfs_inode_id.
- * We go the other direction by fetching inodes.
- *
- * Mapping: sqfs_inode_num <=> fuse_ino_t
- *   Most num(N) maps to N + 1
- *   num(root) maps to FUSE_ROOT_ID == 1
- *   num(0) maps to num(root) + 1
- *
- * FIXME:
- * - Theoretically this could overflow if a filesystem uses all 2 ** 32 inodes,
- *   since fuse inode zero is unavailable.
- */
-#define SQFS_ICACHE_INITIAL 32
-
-#define FUSE_INODE_NONE 0
-#define SQFS_INODE_NONE 1
-
-typedef struct {
-	sqfs_inode_num root;
-	sqfs_hash icache;
-} sqfs_ll_inode_map;
-
-/* Pack tightly to save memory */
-typedef struct {
-	uint32_t refcount;
-	uint32_t ino_hi;
-	uint16_t ino_lo;
-} sqfs_ll_inode_entry;
-
-#define IE_INODE(ie) (((uint64_t)(ie)->ino_hi << 16) | (ie)->ino_lo)
-#define INODE_HI(i) ((i) >> 16)
-#define INODE_LO(i) ((i) & 0xFFFF)
-
-static fuse_ino_t sqfs_ll_ino32_num2fuse(sqfs_ll *ll, sqfs_inode_num n) {
-	sqfs_ll_inode_map *map = ll->ino_data;
-	if (n == map->root) {
-		return FUSE_ROOT_ID;
-	} else if (n == 0) {
-		return map->root + 1;
-	} else {
-		return n + 1;
-	} 
-}
-
-static fuse_ino_t sqfs_ll_ino32_fuse2num(sqfs_ll *ll, fuse_ino_t i) {
-	sqfs_ll_inode_map *map = ll->ino_data;
-	if (i == FUSE_ROOT_ID) {
-		return map->root;
-	} else if (i == map->root + 1) {
-		return 0;
-	} else {
-		return i - 1;
-	}
-}
-
-static fuse_ino_t sqfs_ll_ino32_fuse(sqfs_ll *ll, sqfs_inode_id i) {
-	sqfs_inode inode;
-	if (sqfs_inode_get(&ll->fs, &inode, i))
-		return FUSE_INODE_NONE; /* We shouldn't get here! */
-	return sqfs_ll_ino32_num2fuse(ll, inode.base.inode_number);
-}
-
-static sqfs_inode_id sqfs_ll_ino32_sqfs(sqfs_ll *ll, fuse_ino_t i) {
-	sqfs_ll_inode_map *map;
-	sqfs_inode_num n;
-	sqfs_ll_inode_entry *ie;
-	
-	if (i == FUSE_ROOT_ID)
-		return ll->fs.sb.root_inode;
-	
-	map = ll->ino_data;
-	n = sqfs_ll_ino32_fuse2num(ll, i);
-	
-	ie = sqfs_hash_get(&map->icache, n);
-	return ie ? IE_INODE(ie) : SQFS_INODE_NONE;
-}
-
-static fuse_ino_t sqfs_ll_ino32_fuse_num(sqfs_ll *ll, sqfs_dir_entry *e) {
-	return sqfs_ll_ino32_num2fuse(ll, sqfs_dentry_inode_num(e));
-}
-
-static fuse_ino_t sqfs_ll_ino32_register(sqfs_ll *ll, sqfs_dir_entry *e) {
-	sqfs_ll_inode_map *map = ll->ino_data;
-	
-	sqfs_ll_inode_entry *ie = sqfs_hash_get(&map->icache,
-		sqfs_dentry_inode_num(e));
-	if (ie) {
-		++ie->refcount;
-	} else {
-		sqfs_err err = SQFS_OK;
-		sqfs_inode_id i = sqfs_dentry_inode(e);
-		sqfs_ll_inode_entry nie;
-		nie.ino_hi = INODE_HI(i);
-		nie.ino_lo = INODE_LO(i);
-		nie.refcount = 1;
-		err = sqfs_hash_add(&map->icache, sqfs_dentry_inode_num(e), &nie);
-		if (err)
-			return FUSE_INODE_NONE;
-	}
-	
-	return sqfs_ll_ino32_fuse_num(ll, e);
-}
-
-static void sqfs_ll_ino32_forget(sqfs_ll *ll, fuse_ino_t i, size_t refs) {
-	sqfs_ll_inode_map *map = ll->ino_data;
-	sqfs_inode_num n = sqfs_ll_ino32_fuse2num(ll, i);
-	
-	sqfs_ll_inode_entry *ie = sqfs_hash_get(&map->icache, n);
-	if (!ie)
+static void sqfs_ll_op_getattr(fuse_req_t req, fuse_ino_t ino,
+		struct fuse_file_info *fi) {
+	sqfs_ll_i lli;
+	struct stat st;
+	if (sqfs_ll_iget(req, &lli, ino))
 		return;
 	
-	if (ie->refcount > refs) {
-		ie->refcount -= refs;
+	if (sqfs_stat(&lli.ll->fs, &lli.inode, &st)) {
+		fuse_reply_err(req, ENOENT);
 	} else {
-		sqfs_hash_remove(&map->icache, n);
+		st.st_ino = ino;
+		fuse_reply_attr(req, &st, SQFS_TIMEOUT);
 	}
 }
 
-static void sqfs_ll_ino32_destroy(sqfs_ll *ll) {
-	sqfs_ll_inode_map *map = ll->ino_data;
-	sqfs_hash_destroy(&map->icache);
-	free(map);
+static void sqfs_ll_op_opendir(fuse_req_t req, fuse_ino_t ino,
+		struct fuse_file_info *fi) {
+	sqfs_ll_i *lli;
+	
+	fi->fh = (intptr_t)NULL;
+	
+	lli = malloc(sizeof(*lli));
+	if (!lli) {
+		fuse_reply_err(req, ENOMEM);
+		return;
+	}
+	
+	if (sqfs_ll_iget(req, lli, ino) == SQFS_OK) {
+		if (!S_ISDIR(lli->inode.base.mode)) {
+			fuse_reply_err(req, ENOTDIR);
+		} else {
+			fi->fh = (intptr_t)lli;
+			fuse_reply_open(req, fi);
+			return;
+		}
+	}
+	free(lli);
 }
 
-static sqfs_err sqfs_ll_ino32_init(sqfs_ll *ll) {
-	sqfs_inode inode;
-	sqfs_ll_inode_map *map;
-	sqfs_err err = sqfs_inode_get(&ll->fs, &inode, ll->fs.sb.root_inode);
+static void sqfs_ll_op_releasedir(fuse_req_t req, fuse_ino_t ino,
+		struct fuse_file_info *fi) {
+	free((sqfs_ll_i*)(intptr_t)fi->fh);
+	fuse_reply_err(req, 0); /* yes, this is necessary */
+}
+
+static size_t sqfs_ll_add_direntry(fuse_req_t req, char *buf, size_t bufsize,
+		const char *name, const struct stat *st, off_t off) {
+	#if HAVE_DECL_FUSE_ADD_DIRENTRY
+		return fuse_add_direntry(req, buf, bufsize, name, st, off);
+	#else
+		size_t esize = fuse_dirent_size(strlen(name));
+		if (bufsize >= esize)
+			fuse_add_dirent(buf, name, st, off);
+		return esize;
+	#endif
+}
+static void sqfs_ll_op_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
+		off_t off, struct fuse_file_info *fi) {
+	sqfs_err sqerr;
+	sqfs_dir dir;
+	sqfs_name namebuf;
+	sqfs_dir_entry entry;
+	size_t esize;
+	struct stat st;
+	
+	char *buf = NULL, *bufpos = NULL;
+	sqfs_ll_i *lli = (sqfs_ll_i*)(intptr_t)fi->fh;
+	int err = 0;
+	
+	if (sqfs_dir_open(&lli->ll->fs, &lli->inode, &dir, off))
+		err = EINVAL;
+	if (!err && !(bufpos = buf = malloc(size)))
+		err = ENOMEM;
+	
+	if (!err) {
+		memset(&st, 0, sizeof(st));
+		sqfs_dentry_init(&entry, namebuf);
+		while (sqfs_dir_next(&lli->ll->fs, &dir, &entry, &sqerr)) {
+			st.st_ino = lli->ll->ino_fuse_num(lli->ll, &entry);
+			st.st_mode = sqfs_dentry_mode(&entry);
+		
+			esize = sqfs_ll_add_direntry(req, bufpos, size, sqfs_dentry_name(&entry),
+				&st, sqfs_dentry_next_offset(&entry));
+			if (esize > size)
+				break;
+		
+			bufpos += esize;
+			size -= esize;
+		}
+		if (sqerr)
+			err = EIO;
+	}
+	
 	if (err)
-		return err;
-		
-	map = malloc(sizeof(sqfs_ll_inode_map));
-	map->root = inode.base.inode_number;
-	sqfs_hash_init(&map->icache, sizeof(sqfs_ll_inode_entry),
-		SQFS_ICACHE_INITIAL);
-		
-	ll->ino_fuse = sqfs_ll_ino32_fuse;
-	ll->ino_sqfs = sqfs_ll_ino32_sqfs;
-	ll->ino_fuse_num = sqfs_ll_ino32_fuse_num;
-	ll->ino_register = sqfs_ll_ino32_register;
-	ll->ino_forget = sqfs_ll_ino32_forget;
-	ll->ino_destroy = sqfs_ll_ino32_destroy;
-	ll->ino_data = map;
-	
-	return err; 
+		fuse_reply_err(req, err);
+	else
+		fuse_reply_buf(req, buf, bufpos - buf);
+	free(buf);
 }
 
-
-
-/***** INODE CONVERSION FOR 32-BIT INODES, EXPORT TABLE AVAILABLE ****
- *
- * Same transformation as regular 32-bit, but no caching.
- */
-static sqfs_inode_id sqfs_ll_ino32exp_sqfs(sqfs_ll *ll, fuse_ino_t i) {
-	sqfs_inode_num n;
-	sqfs_inode_id ret;
-	sqfs_err err = SQFS_OK;
-	
-	if (i == FUSE_ROOT_ID)
-		return ll->fs.sb.root_inode;
-	
-	n = sqfs_ll_ino32_fuse2num(ll, i);
-	err = sqfs_export_inode(&ll->fs, n, &ret);
-	return err ? SQFS_INODE_NONE : ret;
-}
-
-static void sqfs_ll_ino32exp_destroy(sqfs_ll *ll) {
-	free(ll->ino_data);
-}
-
-static sqfs_err sqfs_ll_ino32exp_init(sqfs_ll *ll) {
+static void sqfs_ll_op_lookup(fuse_req_t req, fuse_ino_t parent,
+		const char *name) {
+	sqfs_ll_i lli;
+	sqfs_err sqerr;
+	sqfs_name namebuf;
+	sqfs_dir_entry entry;
+	bool found;
 	sqfs_inode inode;
-	sqfs_ll_inode_map *map;
-	sqfs_err err = sqfs_inode_get(&ll->fs, &inode, ll->fs.sb.root_inode);
-	if (err)
-		return err;
-		
-	map = malloc(sizeof(sqfs_ll_inode_map));
-	map->root = inode.base.inode_number;
 	
-	ll->ino_fuse = sqfs_ll_ino32_fuse;
-	ll->ino_sqfs = sqfs_ll_ino32exp_sqfs;
-	ll->ino_fuse_num = sqfs_ll_ino32_fuse_num;
-	ll->ino_destroy = sqfs_ll_ino32exp_destroy;
-	ll->ino_data = map;
+	if (sqfs_ll_iget(req, &lli, parent))
+		return;
 	
-	return err;
-}
-
-
-
-static void sqfs_ll_null_forget(sqfs_ll *ll, fuse_ino_t i, size_t refs) {
-	/* pass */
-}
-
-sqfs_err sqfs_ll_init(sqfs_ll *ll) {
-	sqfs_err err = SQFS_OK;	
-	if (sizeof(fuse_ino_t) >= SQFS_INODE_ID_BYTES) {
-		err = sqfs_ll_ino64_init(ll);
-	} else if (sqfs_export_ok(&ll->fs)) {
-		err = sqfs_ll_ino32exp_init(ll);
+	if (!S_ISDIR(lli.inode.base.mode)) {
+		fuse_reply_err(req, ENOTDIR);
+		return;
+	}
+	
+	sqfs_dentry_init(&entry, namebuf);
+	sqerr = sqfs_dir_lookup(&lli.ll->fs, &lli.inode, name, strlen(name), &entry,
+		&found);
+	if (sqerr) {
+		fuse_reply_err(req, EIO);
+		return;
+	}
+	if (!found) {
+		fuse_reply_err(req, ENOENT);
+		return;
+	}
+	
+	if (sqfs_inode_get(&lli.ll->fs, &inode, sqfs_dentry_inode(&entry))) {
+		fuse_reply_err(req, ENOENT);
 	} else {
-		err = sqfs_ll_ino32_init(ll);
+		struct fuse_entry_param fentry;
+		memset(&fentry, 0, sizeof(fentry));
+		if (sqfs_stat(&lli.ll->fs, &inode, &fentry.attr)) {
+			fuse_reply_err(req, EIO);
+		} else {
+			fentry.attr_timeout = fentry.entry_timeout = SQFS_TIMEOUT;
+			fentry.ino = lli.ll->ino_register(lli.ll, &entry);
+			fentry.attr.st_ino = fentry.ino;
+			fuse_reply_entry(req, &fentry);
+		}
 	}
-	if (!ll->ino_register)
-		ll->ino_register = ll->ino_fuse_num;
-	if (!ll->ino_forget)
-		ll->ino_forget = sqfs_ll_null_forget;
+}
+
+static void sqfs_ll_op_open(fuse_req_t req, fuse_ino_t ino,
+		struct fuse_file_info *fi) {
+	sqfs_inode *inode;
+	sqfs_ll *ll;
 	
-	return err;
+	if (fi->flags & (O_WRONLY | O_RDWR)) {
+		fuse_reply_err(req, EROFS);
+		return;
+	}
+	
+	inode = malloc(sizeof(sqfs_inode));
+	if (!inode) {
+		fuse_reply_err(req, ENOMEM);
+		return;
+	}
+	
+	ll = fuse_req_userdata(req);
+	if (sqfs_ll_inode(ll, inode, ino)) {
+		fuse_reply_err(req, ENOENT);
+	} else if (!S_ISREG(inode->base.mode)) {
+		fuse_reply_err(req, EISDIR);
+	} else {
+		fi->fh = (intptr_t)inode;
+		fuse_reply_open(req, fi);
+		return;
+	}
+	free(inode);
 }
 
-void sqfs_ll_destroy(sqfs_ll *ll) {
-	sqfs_destroy(&ll->fs);
-	if (ll->ino_destroy)
-		ll->ino_destroy(ll);
+static void sqfs_ll_op_release(fuse_req_t req, fuse_ino_t ino,
+		struct fuse_file_info *fi) {
+	free((sqfs_inode*)(intptr_t)fi->fh);
+	fi->fh = 0;
+	fuse_reply_err(req, 0);
 }
 
-sqfs_err sqfs_ll_inode(sqfs_ll *ll, sqfs_inode *inode, fuse_ino_t i) {
-	return sqfs_inode_get(&ll->fs, inode, ll->ino_sqfs(ll, i));
-}
-
-
-sqfs_err sqfs_ll_iget(fuse_req_t req, sqfs_ll_i *lli, fuse_ino_t i) {
+static void sqfs_ll_op_read(fuse_req_t req, fuse_ino_t ino,
+		size_t size, off_t off, struct fuse_file_info *fi) {
+	sqfs_ll *ll = fuse_req_userdata(req);
+	sqfs_inode *inode = (sqfs_inode*)(intptr_t)fi->fh;
 	sqfs_err err = SQFS_OK;
-	lli->ll = fuse_req_userdata(req);
-	if (i != SQFS_FUSE_INODE_NONE) {
-		err = sqfs_ll_inode(lli->ll, &lli->inode, i);
-		if (err)
-			fuse_reply_err(req, ENOENT);
+	
+	off_t osize;
+	char *buf = malloc(size);
+	if (!buf) {
+		fuse_reply_err(req, ENOMEM);
+		return;
 	}
-	return err;
+	
+	osize = size;
+	err = sqfs_read_range(&ll->fs, inode, off, &osize, buf);
+	if (err) {
+		fuse_reply_err(req, EIO);
+	} else if (osize == 0) { /* EOF */
+		fuse_reply_buf(req, NULL, 0);
+	} else {
+		fuse_reply_buf(req, buf, osize);
+	}
+	free(buf);
 }
 
-sqfs_err sqfs_ll_stat(sqfs_ll *ll, sqfs_inode *inode, struct stat *st) {
-	sqfs_err err = SQFS_OK;
-	uid_t id;
+static void sqfs_ll_op_readlink(fuse_req_t req, fuse_ino_t ino) {
+	char *dst;
+	size_t size;
+	sqfs_ll_i lli;
+	if (sqfs_ll_iget(req, &lli, ino))
+		return;
 	
-	memset(st, 0, sizeof(*st));
-	st->st_mode = inode->base.mode;
-	st->st_nlink = inode->nlink;
-	st->st_mtime = st->st_ctime = st->st_atime = inode->base.mtime;
+	if (!S_ISLNK(lli.inode.base.mode)) {
+		fuse_reply_err(req, EINVAL);
+	} else if (sqfs_readlink(&lli.ll->fs, &lli.inode, NULL, &size)) {
+		fuse_reply_err(req, EIO);
+	} else if (!(dst = malloc(size + 1))) {
+		fuse_reply_err(req, ENOMEM);
+	} else if (sqfs_readlink(&lli.ll->fs, &lli.inode, dst, &size)) {
+		fuse_reply_err(req, EIO);
+		free(dst);
+	} else {
+		fuse_reply_readlink(req, dst);
+		free(dst);
+	}
+}
+
+static void sqfs_ll_op_listxattr(fuse_req_t req, fuse_ino_t ino, size_t size) {
+	sqfs_ll_i lli;
+	char *buf;
+	int ferr;
 	
-	if (S_ISREG(st->st_mode)) {
-		/* FIXME: do symlinks, dirs, etc have a size? */
-		st->st_size = inode->xtra.reg.file_size;
-		st->st_blocks = st->st_size / 512;
-	} else if (S_ISBLK(st->st_mode) || S_ISCHR(st->st_mode)) {
-		st->st_rdev = sqfs_makedev(inode->xtra.dev.major,
-			inode->xtra.dev.minor);
+	if (sqfs_ll_iget(req, &lli, ino))
+		return;
+
+	buf = NULL;
+	if (size && !(buf = malloc(size))) {
+		fuse_reply_err(req, ENOMEM);
+		return;
 	}
 	
-	st->st_blksize = ll->fs.sb.block_size; /* seriously? */
+	ferr = sqfs_listxattr(&lli.ll->fs, &lli.inode, buf, &size);
+	if (ferr) {
+		fuse_reply_err(req, ferr);
+	} else if (buf) {
+		fuse_reply_buf(req, buf, size);
+	} else {
+		fuse_reply_xattr(req, size);
+	}
+	free(buf);
+}
+
+static void sqfs_ll_op_getxattr(fuse_req_t req, fuse_ino_t ino,
+		const char *name, size_t size
+#ifdef FUSE_XATTR_POSITION
+		, uint32_t position
+#endif
+		) {
+	sqfs_ll_i lli;
+	char *buf = NULL;
+	size_t real = size;
+
+#ifdef FUSE_XATTR_POSITION
+	if (position != 0) { /* We don't support resource forks */
+		fuse_reply_err(req, EINVAL);
+		return;
+	}
+#endif
 	
-	err = sqfs_id_get(&ll->fs, inode->base.uid, &id);
-	if (err)
-		return err;
-	st->st_uid = id;
-	err = sqfs_id_get(&ll->fs, inode->base.guid, &id);
-	st->st_gid = id;
-	if (err)
-		return err;
+	if (sqfs_ll_iget(req, &lli, ino))
+		return;
 	
-	return SQFS_OK;
+	if (!(buf = malloc(size)))
+		fuse_reply_err(req, ENOMEM);
+	else if (sqfs_xattr_lookup(&lli.ll->fs, &lli.inode, name, buf, &real))
+		fuse_reply_err(req, EIO);
+	else if (real == 0)
+		fuse_reply_err(req, sqfs_enoattr());
+	else if (size == 0)
+		fuse_reply_xattr(req, real);
+	else if (size < real)
+		fuse_reply_err(req, ERANGE);
+	else
+		fuse_reply_buf(req, buf, real);
+	free(buf);
+}
+
+static void sqfs_ll_op_forget(fuse_req_t req, fuse_ino_t ino,
+		unsigned long nlookup) {
+	sqfs_ll_i lli;
+	sqfs_ll_iget(req, &lli, SQFS_FUSE_INODE_NONE);
+	lli.ll->ino_forget(lli.ll, ino, nlookup);
+	fuse_reply_none(req);
+}
+
+
+/* Helpers to abstract out FUSE 2.5 vs 2.6+ differences */
+
+typedef struct {
+	int fd;
+	struct fuse_chan *ch;
+} sqfs_ll_chan;
+
+static sqfs_err sqfs_ll_mount(sqfs_ll_chan *ch, const char *mountpoint,
+		struct fuse_args *args) {
+	#ifdef HAVE_NEW_FUSE_UNMOUNT
+		ch->ch = fuse_mount(mountpoint, args);
+	#else
+		ch->fd = fuse_mount(mountpoint, args);
+		if (ch->fd == -1)
+			return SQFS_ERR;
+		ch->ch = fuse_kern_chan_new(ch->fd);
+	#endif
+	return ch->ch ? SQFS_OK : SQFS_ERR;
+}
+
+static void sqfs_ll_unmount(sqfs_ll_chan *ch, const char *mountpoint) {
+	#ifdef HAVE_NEW_FUSE_UNMOUNT
+		fuse_unmount(mountpoint, ch->ch);
+	#else
+		close(ch->fd);
+		fuse_unmount(mountpoint);
+	#endif
+}
+
+static sqfs_ll *sqfs_ll_open(const char *path) {
+	sqfs_ll *ll;
+	
+	ll = malloc(sizeof(*ll));
+	if (!ll) {
+		perror("Can't allocate memory");
+	} else {
+		memset(ll, 0, sizeof(*ll));
+	
+		if (sqfs_open_image(&ll->fs, path) == SQFS_OK) {
+			if (sqfs_ll_init(ll))
+				fprintf(stderr, "Can't initialize this filesystem!\n");
+			else
+				return ll;
+			sqfs_destroy(&ll->fs);
+		}
+		
+		free(ll);
+	}
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
+	struct fuse_args args;
+	sqfs_opts opts;
+	
+	char *mountpoint = NULL;
+	int mt, fg;
+	
+	int err;
+	sqfs_ll *ll;
+	
+	struct fuse_lowlevel_ops sqfs_ll_ops;
+	memset(&sqfs_ll_ops, 0, sizeof(sqfs_ll_ops));
+	sqfs_ll_ops.getattr		= sqfs_ll_op_getattr;
+	sqfs_ll_ops.opendir		= sqfs_ll_op_opendir;
+	sqfs_ll_ops.releasedir	= sqfs_ll_op_releasedir;
+	sqfs_ll_ops.readdir		= sqfs_ll_op_readdir;
+	sqfs_ll_ops.lookup		= sqfs_ll_op_lookup;
+	sqfs_ll_ops.open		= sqfs_ll_op_open;
+	sqfs_ll_ops.release		= sqfs_ll_op_release;
+	sqfs_ll_ops.read		= sqfs_ll_op_read;
+	sqfs_ll_ops.readlink	= sqfs_ll_op_readlink;
+	sqfs_ll_ops.listxattr	= sqfs_ll_op_listxattr;
+	sqfs_ll_ops.getxattr	= sqfs_ll_op_getxattr;
+	sqfs_ll_ops.forget		= sqfs_ll_op_forget;
+   
+	/* PARSE ARGS */
+	args.argc = argc;
+	args.argv = argv;
+	args.allocated = 0;
+	
+	opts.progname = argv[0];
+	opts.image = NULL;
+	opts.mountpoint = 0;
+	if (fuse_opt_parse(&args, &opts, NULL, sqfs_opt_proc) == -1)
+		sqfs_usage(argv[0], true);
+
+	if (fuse_parse_cmdline(&args, &mountpoint, &mt, &fg) == -1)
+		sqfs_usage(argv[0], true);
+	if (mountpoint == NULL)
+		sqfs_usage(argv[0], true);
+	
+	/* OPEN FS */
+	err = !(ll = sqfs_ll_open(opts.image));
+	
+	/* STARTUP FUSE */
+	if (!err) {
+		sqfs_ll_chan ch;
+		err = -1;
+		if (sqfs_ll_mount(&ch, mountpoint, &args) == SQFS_OK) {
+			struct fuse_session *se = fuse_lowlevel_new(&args,
+				&sqfs_ll_ops, sizeof(sqfs_ll_ops), ll);	
+			if (se != NULL) {
+				if (sqfs_ll_daemonize(fg) != -1) {
+					if (fuse_set_signal_handlers(se) != -1) {
+						fuse_session_add_chan(se, ch.ch);
+						/* FIXME: multithreading */
+						err = fuse_session_loop(se);
+						fuse_remove_signal_handlers(se);
+						#if HAVE_DECL_FUSE_SESSION_REMOVE_CHAN
+							fuse_session_remove_chan(ch.ch);
+						#endif
+					}
+				}
+				fuse_session_destroy(se);
+			}
+			sqfs_ll_destroy(ll);
+			sqfs_ll_unmount(&ch, mountpoint);
+		}
+	}
+	fuse_opt_free_args(&args);
+	free(mountpoint);
+	
+	return -err;
 }
