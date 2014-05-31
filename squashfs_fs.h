0e3bffd2958ce6a3e44f8ea0edf6dfb3c4bdc84c
Dave Vasilevsky <dave@vasilevsky.ca>
2014-05-31 03:52:01 -0400

    Update Makefile
    - Include swap.*.inc in distribution, since Windows has no sed
    - Force build swap.*.inc, even when not making ‘all’
    - Modify squashfs_fs.h for Windows
    - Update TODOs

--- squashfs_fs.h
+++ squashfs_fs.h
@@ -153,7 +153,6 @@ struct squashfs_dir_index {
 	__le32			index;
 	__le32			start_block;
 	__le32			size;
-	unsigned char		name[0];
 };
 
 struct squashfs_base_inode {
@@ -218,7 +217,6 @@ struct squashfs_symlink_inode {
 	__le32			inode_number;
 	__le32			nlink;
 	__le32			symlink_size;
-	char			symlink[0];
 };
 
 struct squashfs_reg_inode {
@@ -232,7 +230,6 @@ struct squashfs_reg_inode {
 	__le32			fragment;
 	__le32			offset;
 	__le32			file_size;
-	__le16			block_list[0];
 };
 
 struct squashfs_lreg_inode {
@@ -249,7 +246,6 @@ struct squashfs_lreg_inode {
 	__le32			fragment;
 	__le32			offset;
 	__le32			xattr;
-	__le16			block_list[0];
 };
 
 struct squashfs_dir_inode {
@@ -280,7 +276,6 @@ struct squashfs_ldir_inode {
 	__le16			i_count;
 	__le16			offset;
 	__le32			xattr;
-	struct squashfs_dir_index	index[0];
 };
 
 struct squashfs_dir_entry {
@@ -288,7 +283,6 @@ struct squashfs_dir_entry {
 	__le16			inode_number;
 	__le16			type;
 	__le16			size;
-	char			name[0];
 };
 
 struct squashfs_dir_header {
@@ -306,12 +300,10 @@ struct squashfs_fragment_entry {
 struct squashfs_xattr_entry {
 	__le16			type;
 	__le16			size;
-	char			data[0];
 };
 
 struct squashfs_xattr_val {
 	__le32			vsize;
-	char			value[0];
 };
 
 struct squashfs_xattr_id {
