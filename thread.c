df8482dbef4e84ea1b56c218e353ac601e707b41
Dave Vasilevsky <dave@vasilevsky.ca>
2014-06-09 01:38:28 -0400

    Unused vars in non-threaded mode

--- thread.c
+++ thread.c
@@ -54,14 +54,17 @@
 		return pthread_cond_broadcast(cv) ? SQFS_ERR : SQFS_OK;
 	}
 #else
-	sqfs_err sqfs_mutex_init(sqfs_mutex *m) { return SQFS_OK; }
-	sqfs_err sqfs_mutex_destroy(sqfs_mutex *m) { return SQFS_OK; }
-	sqfs_err sqfs_mutex_lock(sqfs_mutex *m) { return SQFS_OK; }
-	sqfs_err sqfs_mutex_unlock(sqfs_mutex *m) { return SQFS_OK; }
+	sqfs_err sqfs_mutex_init(sqfs_mutex *SQFS_UNUSED(m)) { return SQFS_OK; }
+	sqfs_err sqfs_mutex_destroy(sqfs_mutex *SQFS_UNUSED(m)) { return SQFS_OK; }
+	sqfs_err sqfs_mutex_lock(sqfs_mutex *SQFS_UNUSED(m)) { return SQFS_OK; }
+	sqfs_err sqfs_mutex_unlock(sqfs_mutex *SQFS_UNUSED(m)) { return SQFS_OK; }
 
-	sqfs_err sqfs_cond_init(sqfs_cond_var *cv) { return SQFS_OK; }
-	sqfs_err sqfs_cond_destroy(sqfs_cond_var *cv) { return SQFS_OK; }
-	sqfs_err sqfs_cond_wait(sqfs_cond_var *cv, sqfs_mutex *m) { return SQFS_OK; }
-	sqfs_err sqfs_cond_signal(sqfs_cond_var *cv) { return SQFS_OK; }
-	sqfs_err sqfs_cond_broadcast(sqfs_cond_var *cv) { return SQFS_OK; }
+	sqfs_err sqfs_cond_init(sqfs_cond_var *SQFS_UNUSED(cv)) { return SQFS_OK; }
+	sqfs_err sqfs_cond_destroy(sqfs_cond_var *SQFS_UNUSED(cv))
+    { return SQFS_OK; }
+	sqfs_err sqfs_cond_wait(sqfs_cond_var *SQFS_UNUSED(cv),
+    sqfs_mutex *SQFS_UNUSED(m)) { return SQFS_OK; }
+	sqfs_err sqfs_cond_signal(sqfs_cond_var *SQFS_UNUSED(cv)) { return SQFS_OK; }
+	sqfs_err sqfs_cond_broadcast(sqfs_cond_var *SQFS_UNUSED(cv))
+    { return SQFS_OK; }
 #endif
