c33c1122b3c96466fd0f278b2c609900e18a6271
Dave Vasilevsky <dave@vasilevsky.ca>
2014-07-09 23:44:12 -0400

    Resolve symlinks

--- win/dokanx.cpp
+++ win/dokanx.cpp
@@ -2,9 +2,13 @@
 #include "squashfuse.h"
 
 #include <shlwapi.h>
+#include <stack>
+#include <queue>
 
+// Shortcut to get the global context
 #define SQCONTEXT ((sqfs_dokan*)fi->DokanOptions->GlobalContext)
 
+// Global context
 struct sqfs_dokan {
   sqfs fs;
   sqfs_inode root;
@@ -12,15 +16,93 @@ struct sqfs_dokan {
   bool escape; // Escape illegal characters?
 };
 
-static const wchar_t *ILLEGAL_CHARS = L"<>:\"/\\|?*";
-static const wchar_t ESCAPE = L'%';
+// Characters that are illegal in filenames
+static const wchar_t *SQ_ILLEGAL_CHARS = L"<>:\"/\\|?*";
+// Character for escaping illegal characters
+static const wchar_t SQ_ESCAPE = L'%';
 
-static wchar_t sqfs_hexchar(int v) {
+// Escape a filename. Return empty string if escape is false and illegal
+// characters are present.
+static std::wstring sqfs_dk_escape(const std::wstring &s, bool escape);
+// Unescape a filename
+static std::wstring sqfs_dk_unescape(const std::wstring &s);
+
+// 0-16 to hex char
+static wchar_t sqfs_dk_hexchar(int v);
+// hex character to integer, or -1 if invalid character
+static int sqfs_dk_hexdigit(wchar_t h);
+
+// Convert path from Win to squashfs internal
+static std::string sqfs_dk_sqfs_path(LPCWSTR path, bool escape);
+// Convert filename from squashfs to Win. Return empty on failure
+static std::wstring sqfs_dk_win_name(const char *name, bool escape);
+
+// Convert a time_t to a FILETIME
+static void sqfs_dk_filetime(time_t t, LPFILETIME pft);
+
+// Get the target of a symlink
+static sqfs_err sqfs_dk_readlink(sqfs *fs, sqfs_inode *inode,
+  std::string &target);
+
+// Generate a good volume name for this image
+static std::wstring sqfs_dk_volname(const wchar_t *image);
+// Generate a serial number for this fs
+static DWORD sqfs_dk_serial_number(sqfs &fs);
+
+
+// Resolver for file paths
+struct sqfs_dk_resolver {
+  NTSTATUS status;
+
+  // Get the inode for the looked up path, if it exists
+  sqfs_inode& target() { return inodes.top(); }
+
+  // Resolve a path
+  NTSTATUS resolve(const char *path);
+
+  // Constructor for ops
+  sqfs_dk_resolver(sqfs_dokan *ctx, LPCWSTR path);
+
+private:
+  std::stack<sqfs_inode> inodes;
+  sqfs *fs;
+
+  // Split a path
+  typedef std::queue<std::string> components;
+  components split(const char *path);
+};
+
+
+// Fill a file information structure from an inode
+template <typename T>
+static void sqfs_dk_file_info(T *info, sqfs_inode *inode,
+    sqfs_dir_entry *entry) {
+  info->dwFileAttributes = 0;
+  if (inode && S_ISDIR(inode->base.mode))
+    info->dwFileAttributes |= FILE_ATTRIBUTE_DIRECTORY;
+  if (entry && S_ISDIR(sqfs_dentry_mode(entry)))
+    info->dwFileAttributes |= FILE_ATTRIBUTE_DIRECTORY;
+  info->dwFileAttributes |= FILE_ATTRIBUTE_READONLY;
+
+  time_t mtime = inode ? inode->base.mtime : 0;
+  sqfs_dk_filetime(mtime, &info->ftLastWriteTime);
+  info->ftCreationTime = info->ftLastWriteTime;
+  info->ftLastAccessTime = info->ftLastWriteTime;
+
+  if (inode && S_ISREG(inode->base.mode)) {
+    info->nFileSizeHigh = inode->xtra.reg.file_size >> 32;
+    info->nFileSizeLow = (DWORD)inode->xtra.reg.file_size;
+  } else {
+    info->nFileSizeHigh = info->nFileSizeLow = 0;
+  }
+}
+
+static wchar_t sqfs_dk_hexchar(int v) {
   if (v < 10)
     return L'0' + v;
   return L'A' + (v - 10);
 }
-static int sqfs_hexdigit(wchar_t h) {
+static int sqfs_dk_hexdigit(wchar_t h) {
   if (h >= L'A' && h <= L'F')
     return h - L'A' + 10;
   if (h >= L'a' && h <= L'f')
@@ -30,11 +112,11 @@ static int sqfs_hexdigit(wchar_t h) {
   return -1;
 }
 
-static std::wstring sqfs_escape(const std::wstring &s, bool escape) {
+static std::wstring sqfs_dk_escape(const std::wstring &s, bool escape) {
   std::wstring ret;
-  for (auto it = s.begin(); it != s.end(); ++it) {
+  for (std::wstring::const_iterator it = s.begin(); it != s.end(); ++it) {
     bool illegal = false;
-    for (const wchar_t *c = ILLEGAL_CHARS; *c; ++c) {
+    for (const wchar_t *c = SQ_ILLEGAL_CHARS; *c; ++c) {
       if (*it == *c) {
         illegal  = true;
         break;
@@ -43,25 +125,25 @@ static std::wstring sqfs_escape(const std::wstring &s, bool escape) {
 
     if (illegal && !escape)
       return std::wstring(); // Reject unescaped illegal chars
-    if (illegal || (escape && *it == ESCAPE)) {
-      ret.push_back(ESCAPE);
-      ret.push_back(sqfs_hexchar(*it / 16));
-      ret.push_back(sqfs_hexchar(*it & 0xf));
+    if (illegal || (escape && *it == SQ_ESCAPE)) {
+      ret.push_back(SQ_ESCAPE);
+      ret.push_back(sqfs_dk_hexchar(*it / 16));
+      ret.push_back(sqfs_dk_hexchar(*it & 0xf));
     } else {
       ret.push_back(*it);
     }
   }
   return ret;
 }
-static std::wstring sqfs_unescape(const std::wstring &s) {
+static std::wstring sqfs_dk_unescape(const std::wstring &s) {
   std::wstring ret;
-  for (auto it = s.begin(); it != s.end(); ++it) {
-    if (*it == ESCAPE) {
+  for (std::wstring::const_iterator it = s.begin(); it != s.end(); ++it) {
+    if (*it == SQ_ESCAPE) {
       int a = -1, b = -1;
       if (it++ != s.end())
-        a = sqfs_hexdigit(*it);
+        a = sqfs_dk_hexdigit(*it);
       if (it++ != s.end())
-        b = sqfs_hexdigit(*it);
+        b = sqfs_dk_hexdigit(*it);
       if (a != -1 && b != -1)
         ret.push_back(a * 16 + b);
     } else {
@@ -71,15 +153,11 @@ static std::wstring sqfs_unescape(const std::wstring &s) {
   return ret;
 }
 
-// Convert filename from squashfs to Win. Return empty on failure
-
-
-// Convert path from Win to squashfs internal
 static std::string sqfs_dk_sqfs_path(LPCWSTR path, bool escape) {
   // Unescape characters
   std::wstring wname = path;
   if (escape)
-    wname = sqfs_unescape(wname);
+    wname = sqfs_dk_unescape(wname);
 
   // Convert to UTF8
   char *buf = sqfs_str_utf8(wname.c_str());
@@ -87,7 +165,7 @@ static std::string sqfs_dk_sqfs_path(LPCWSTR path, bool escape) {
   free(buf);
 
   // Convert separators
-  for (auto it = ret.begin(); it != ret.end(); ++it) {
+  for (std::string::iterator it = ret.begin(); it != ret.end(); ++it) {
     if (*it == '\\')
       *it = '/';
   }
@@ -101,59 +179,131 @@ static std::wstring sqfs_dk_win_name(const char *name, bool escape) {
   std::wstring wname = buf;
   free(buf);
 
-  return sqfs_escape(wname, escape);
+  return sqfs_dk_escape(wname, escape);
 }
 
-// Lookup a file
-static NTSTATUS sqfs_dk_lookup(sqfs_dokan *ctx, LPCWSTR path,
-    sqfs_inode &inode) {
-  std::string spath = sqfs_dk_sqfs_path(path, ctx->escape);
-  inode = ctx->root;
-  bool found = false;
-  sqfs_err err = sqfs_lookup_path(&ctx->fs, &inode, spath.c_str(), &found);
+static sqfs_err sqfs_dk_readlink(sqfs *fs, sqfs_inode *inode,
+    std::string &target) {
+  size_t size;
+  sqfs_err err = sqfs_readlink(fs, inode, NULL, &size);
   if (err)
-    return STATUS_INTERNAL_ERROR;
-  if (!found)
-    return STATUS_OBJECT_PATH_NOT_FOUND;
-  return STATUS_SUCCESS;
+    return err;
+  char *buf = new char[size];
+  err = sqfs_readlink(fs, inode, buf, &size);
+  if (err)
+    return err;
+  target = buf;
+  delete[] buf;
+  return SQFS_OK;
 }
 
-// Generate a serial number for this fs
+sqfs_dk_resolver::components sqfs_dk_resolver::split(const char *path) {
+  components cs;
+  if (path[0] == '/') // root
+    cs.push(std::string());
+  while (*path) {
+    while (*path == '/') // skip separators
+      ++path;
+    if (!*path)
+      break;
+
+    const char *sep = strchr(path, '/');
+    if (!sep)
+      sep = path + strlen(path);
+    cs.push(std::string(path, sep));
+    path = sep;
+  }
+  return cs;
+}
+
+NTSTATUS sqfs_dk_resolver::resolve(const char *path) {
+  components cs = split(path);
+  
+  sqfs_dir_entry entry;
+  sqfs_name namebuf;
+  sqfs_dentry_init(&entry, namebuf);
+
+  while (!status && !cs.empty()) {
+    std::string name = cs.front();
+    cs.pop();
+
+    // Handle special cases
+    if (name.empty()) { // root
+      while (inodes.size() > 1)
+        inodes.pop();
+      continue;
+    } else if (name == ".") {
+      continue;
+    } else if (name == "..") {
+      if (inodes.size() > 1)
+        inodes.pop();
+      continue;
+    }
+
+    // Lookup the next path component
+    bool found;
+    sqfs_inode inode = target();
+    sqfs_err err = sqfs_dir_lookup(fs, &inode, name.c_str(), name.size(),
+      &entry, &found);
+    if (err)
+      return (status = STATUS_INTERNAL_ERROR);
+    else if (!found)
+      return (status = STATUS_OBJECT_PATH_NOT_FOUND);
+
+    err = sqfs_inode_get(fs, &inode, sqfs_dentry_inode(&entry));
+    if (err)
+      return (status = STATUS_INTERNAL_ERROR);
+    if (S_ISLNK(sqfs_dentry_mode(&entry))) {
+      // symlink: add target to front of components
+      std::string target;
+      if (sqfs_dk_readlink(fs, &inode, target))
+        return (status = STATUS_INTERNAL_ERROR);
+      if (resolve(target.c_str()))
+        return status;
+    } else {
+      // file or dir: add inode to stack
+      inodes.push(inode);
+    }
+  }
+  return status;
+}
+
+sqfs_dk_resolver::sqfs_dk_resolver(sqfs_dokan *ctx, LPCWSTR path) {
+  fs = &ctx->fs;
+  status = STATUS_SUCCESS;
+  inodes.push(ctx->root);
+
+  std::string spath = sqfs_dk_sqfs_path(path, ctx->escape);
+  resolve(spath.c_str());
+}
+
+
 static DWORD sqfs_dk_serial_number(sqfs &fs) {
   return fs.sb.mkfs_time ^ fs.sb.inodes ^ (DWORD)fs.sb.inode_table_start;
 }
 
-// Convert a time_t to a FILETIME
+static std::wstring sqfs_dk_volname(const wchar_t *image) {
+  wchar_t *name, *buf;
+  name = buf = _wcsdup(image);
+  PathStripPath(name);
+  PathRemoveExtension(name);
+  std::wstring ret = name;
+  free(buf);
+  return ret;
+}
+
 static void sqfs_dk_filetime(time_t t, LPFILETIME pft) {
   LONGLONG ll = Int32x32To64(t, 10000000) + 116444736000000000;
   pft->dwLowDateTime = (DWORD)ll;
   pft->dwHighDateTime = ll >> 32;
 }
 
-// Fill a file information structure from an inode
-template <typename T>
-static void sqfs_dk_file_info(T *info, sqfs_inode &inode) {
-  info->dwFileAttributes = 0;
-  if (S_ISDIR(inode.base.mode))
-    info->dwFileAttributes |= FILE_ATTRIBUTE_DIRECTORY;
-  info->dwFileAttributes |= FILE_ATTRIBUTE_READONLY;
-  sqfs_dk_filetime(inode.base.mtime, &info->ftLastWriteTime);
-  info->ftCreationTime = info->ftLastWriteTime;
-  info->ftLastAccessTime = info->ftLastWriteTime;
-  if (S_ISREG(inode.base.mode)) {
-    info->nFileSizeHigh = inode.xtra.reg.file_size >> 32;
-    info->nFileSizeLow = (DWORD)inode.xtra.reg.file_size;
-  }
-}
-
 static NTSTATUS sqfs_dk_op_create_file(LPCWSTR path, DWORD access,
     DWORD sharemode, DWORD create, DWORD flags, PDOKAN_FILE_INFO fi) {
-  sqfs_inode *inode = new sqfs_inode();
-  NTSTATUS nterr = sqfs_dk_lookup(SQCONTEXT, path, *inode);
-  if (nterr) {
-    delete inode;
-    return nterr;
-  }
+  sqfs_dk_resolver resolver(SQCONTEXT, path);
+  if (resolver.status)
+    return resolver.status;
+  sqfs_inode *inode = new sqfs_inode(resolver.target());
 
   // FIXME: Check if it's a regular file?
 
@@ -162,10 +312,10 @@ static NTSTATUS sqfs_dk_op_create_file(LPCWSTR path, DWORD access,
 }
 
 static NTSTATUS sqfs_dk_op_open_directory(LPCWSTR path, PDOKAN_FILE_INFO fi) {
-  sqfs_inode inode;
-  NTSTATUS nterr = sqfs_dk_lookup(SQCONTEXT, path, inode);
-  if (nterr)
-    return nterr;
+  sqfs_dk_resolver resolver(SQCONTEXT, path);
+  if (resolver.status)
+    return resolver.status;
+  sqfs_inode inode = resolver.target();
 
   if (!S_ISDIR(inode.base.mode))
     return STATUS_NOT_A_DIRECTORY;
@@ -189,13 +339,13 @@ static NTSTATUS sqfs_dk_op_read_file(LPCWSTR path, LPVOID buf, DWORD bufsize,
 
 static NTSTATUS sqfs_dk_op_get_file_information(LPCWSTR path,
     LPBY_HANDLE_FILE_INFORMATION info, PDOKAN_FILE_INFO fi) {
-  sqfs_inode inode;
-  NTSTATUS nterr = sqfs_dk_lookup(SQCONTEXT, path, inode);
-  if (nterr)
-    return nterr;
+  sqfs_dk_resolver resolver(SQCONTEXT, path);
+  if (resolver.status)
+    return resolver.status;
+  sqfs_inode inode = resolver.target();
 
   ZeroMemory(info, sizeof(*info));
-  sqfs_dk_file_info(info, inode);
+  sqfs_dk_file_info(info, &inode, NULL);
   info->nNumberOfLinks = inode.nlink;
   info->nFileIndexLow = inode.base.inode_number;
   info->dwVolumeSerialNumber = sqfs_dk_serial_number(SQCONTEXT->fs);
@@ -205,10 +355,10 @@ static NTSTATUS sqfs_dk_op_get_file_information(LPCWSTR path,
 static NTSTATUS sqfs_dk_op_find_files(LPCWSTR path, PFillFindData filler,
     PDOKAN_FILE_INFO fi) {
   sqfs *fs = &SQCONTEXT->fs;
-  sqfs_inode inode;
-  NTSTATUS nterr = sqfs_dk_lookup(SQCONTEXT, path, inode);
-  if (nterr)
-    return nterr;
+  sqfs_dk_resolver resolver(SQCONTEXT, path);
+  if (resolver.status)
+    return resolver.status;
+  sqfs_inode inode = resolver.target();
 
   if (!S_ISDIR(inode.base.mode))
     return STATUS_NOT_A_DIRECTORY;
@@ -227,17 +377,19 @@ static NTSTATUS sqfs_dk_op_find_files(LPCWSTR path, PFillFindData filler,
   find.cAlternateFileName[0] = L'\0';
 
   sqfs_err err;
-  sqfs_inode child;
   while (sqfs_dir_next(fs, &dir, &entry, &err)) {
     std::wstring name = sqfs_dk_win_name(sqfs_dentry_name(&entry),
       SQCONTEXT->escape);
     if (name.empty())
       continue; // Ignore illegal names
 
-    if (sqfs_inode_get(fs, &child, sqfs_dentry_inode(&entry)))
-      return STATUS_INTERNAL_ERROR;
-
-    sqfs_dk_file_info(&find, child);
+    sqfs_dk_resolver child_resolver(resolver);
+    child_resolver.resolve(sqfs_dentry_name(&entry));
+    // Could fail because of bad symlink
+    sqfs_inode *child = child_resolver.status ? NULL
+      : &child_resolver.target();
+    
+    sqfs_dk_file_info(&find, child, &entry);
     wcscpy_s(find.cFileName, name.c_str());
 
     filler(&find, fi);
@@ -264,17 +416,6 @@ static NTSTATUS sqfs_dk_op_unmount(PDOKAN_FILE_INFO fi) {
   return STATUS_SUCCESS;
 }
 
-
-// Figure out a good volume name for this image
-static std::wstring sqfs_dk_volname(const wchar_t *image) {
-  wchar_t *name, *buf;
-  name = buf = _wcsdup(image);
-  PathStripPath(name);
-  PathRemoveExtension(name);
-  std::wstring ret = name;
-  free(buf);
-  return ret;
-}
 
 static void sqfs_dk_die(const wchar_t *msg) {
   if (msg)
