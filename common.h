de661cbae53cb36215c7b1663132ed854ec319d6
Kevin Vigor <kvigor@fb.com>
2019-02-28 15:49:30 -0700

    Refactor cache API. This is a forst step to multithreading.

--- common.h
+++ common.h
@@ -59,11 +59,24 @@ typedef struct sqfs_inode sqfs_inode;
 typedef struct {
 	size_t size;
 	void *data;
+	int refcount;
 } sqfs_block;
 
 typedef struct {
 	sqfs_off_t block;
 	size_t offset;
 } sqfs_md_cursor;
+
+/* Increment the refcount on the block. */
+static inline void sqfs_block_ref(sqfs_block *block) {
+	__atomic_add_fetch(&block->refcount, 1, __ATOMIC_RELEASE);
+}
+
+/* decrement the refcount on the block, return non-zero if we held the last
+ * reference.
+ */
+static inline int sqfs_block_deref(sqfs_block *block) {
+	return __atomic_sub_fetch(&block->refcount, 1, __ATOMIC_ACQUIRE) == 0;
+}
 
 #endif
