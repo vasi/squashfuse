e7e404365b72936be9264c958ea4ddbbbfacb871
Kevin Vigor <kvigor@gmail.com>
2020-02-06 09:59:56 -0800

    The existing cache API has effectively no locking and relies on the single- threaded nature of the code to prevent contention on cache entries. As a first step to multithreading the driver, refactor the cache API to allow alternative implementations. In this changeset the API is changed but the internal cache implementation is not make thread-safe, so it still suitable only for single-threaded usage. Specific changes:
    *   sqfs_cache type is made opaque.
    *   previously, newly allocated cache entries were assumed valid. This meant that
        on any failure path following a cache entry allocation, one had to be careful
        to call sqfs_cache_invalidate(). The assumption is now reversed, cache
        entries are invalid until explicitly marked valid with
        sqfs_cache_entry_valid(). This simplifies error handling.
    *   block cache code was intermixed with generic cache code, relocate to fs.c
    *   cache eviction led to object destruction in block cache. The only thing
        preventing cache eviction while block object in use is single-threaded
        nature of code. Instead use a refcounting mechanism on the block entries
        so that we can independently manage block lifetime.

---
+++ tests/cachetest.cpp
@@ -0,0 +1,85 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "cache.h"
+}
+
+struct TestStruct {
+    int x;
+    int y;
+};
+
+static void TestStructDispose(void *) {
+    // nada.
+}
+
+TEST(cachetest, CacheMiss) {
+    sqfs_cache cache;
+    TestStruct *entry;
+
+    EXPECT_EQ(sqfs_cache_init(&cache, sizeof(TestStruct), 16,
+                              TestStructDispose), SQFS_OK);
+    entry = (TestStruct *)sqfs_cache_get(&cache, 1);
+    EXPECT_EQ(sqfs_cache_entry_valid(&cache, entry), 0);
+    sqfs_cache_destroy(&cache);
+}
+
+TEST(cachetest, MarkValidAndLookup) {
+    sqfs_cache cache;
+    TestStruct *entry;
+
+    EXPECT_EQ(sqfs_cache_init(&cache, sizeof(TestStruct), 16,
+                              TestStructDispose), SQFS_OK);
+    entry = (TestStruct *)sqfs_cache_get(&cache, 1);
+    entry->x = 666;
+    entry->y = 777;
+    sqfs_cache_entry_mark_valid(&cache, entry);
+    sqfs_cache_put(&cache, entry);
+    EXPECT_NE(sqfs_cache_entry_valid(&cache, entry), 0);
+    entry = (TestStruct *)sqfs_cache_get(&cache, 1);
+    EXPECT_NE(sqfs_cache_entry_valid(&cache, entry), 0);
+    EXPECT_EQ(entry->x, 666);
+    EXPECT_EQ(entry->y, 777);
+    sqfs_cache_put(&cache, entry);
+
+    sqfs_cache_destroy(&cache);
+}
+
+TEST(cachetest, TwoEntries) {
+    sqfs_cache cache;
+    TestStruct *entry1, *entry2;
+
+    EXPECT_EQ(sqfs_cache_init(&cache, sizeof(TestStruct), 16,
+                              TestStructDispose), SQFS_OK);
+
+    entry1 = (TestStruct *)sqfs_cache_get(&cache, 1);
+    entry1->x = 1;
+    entry1->y = 2;
+    sqfs_cache_entry_mark_valid(&cache, entry1);
+    sqfs_cache_put(&cache, entry1);
+
+    entry2 = (TestStruct *)sqfs_cache_get(&cache, 666);
+    entry2->x = 3;
+    entry2->y = 4;
+    sqfs_cache_entry_mark_valid(&cache, entry2);
+    sqfs_cache_put(&cache, entry2);
+
+    entry1 = (TestStruct *)sqfs_cache_get(&cache, 1);
+    sqfs_cache_put(&cache, entry1);
+    entry2 = (TestStruct *)sqfs_cache_get(&cache, 666);
+    sqfs_cache_put(&cache, entry2);
+    EXPECT_NE(sqfs_cache_entry_valid(&cache, entry1), 0);
+    EXPECT_NE(sqfs_cache_entry_valid(&cache, entry2), 0);
+    EXPECT_EQ(entry1->x, 1);
+    EXPECT_EQ(entry1->y, 2);
+    EXPECT_EQ(entry2->x, 3);
+    EXPECT_EQ(entry2->y, 4);
+
+    sqfs_cache_destroy(&cache);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
