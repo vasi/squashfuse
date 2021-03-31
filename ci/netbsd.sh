e9d66bba78bec764ff8a245ad74ee7d46fe21209
Dave Vasilevsky <dave@vasilevsky.ca>
2021-03-31 06:22:25 -0400

    netbsd ci

---
+++ ci/netbsd.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -ex
+
+# Set PATH, PKG_PATH, etc
+. /root/.profile
+
+# Install packages
+pkg_add autoconf automake libtool pkg-config fuse \
+  lz4 lzo zstd \
+  squashfs coreutils # For tests
+
+# Enter our source dir, and print nice data on exit
+cd /squashfuse
+cleanup() {
+  cat config.log || true
+  cat tests/*.log || true
+  mksquashfs || true
+}
+trap "cleanup" TERM
+
+# Build
+./autogen.sh
+LDFLAGS='-L/usr/pkg/lib' CPPFLAGS='-Werror -I/usr/pkg/include' ./configure
+make
+
+# Test
+# Ensure buffers are big enough that perfuse doesn't yield warnings
+sysctl -w kern.sbmax=3000000
+# Run on actual disk, /tmp may not be large
+mkdir /root/tmp
+
+TMPDIR=/root/tmp make check
+diff -u ci/expected-features/all ci/features
+
+# Test install
+make install
