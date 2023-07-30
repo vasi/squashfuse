vasi/squashfuse

commit 53e1b97002c7bc13392a6b0121680d28a94a9715

Add support for mounting of subdirectory
This commit adds ability to mount subdirectory of image with option -o subdir=/dir.

dir.c
@@ -27,6 +27,7 @@
 #include "fs.h"
 #include "swap.h"
 
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
@@ -252,12 +253,12 @@ sqfs_err sqfs_dir_lookup(sqfs *fs, sqfs_inode *inode,
 	sqfs_err err;
 	sqfs_dir dir;
 	sqfs_dir_ff_name_t arg;
-	
+
 	*found = false;
-	
+
 	if ((err = sqfs_dir_open(fs, inode, &dir, 0)))
 		return err;
-	
+
 	/* Fast forward to header */
 	arg.cmp = name;
 	arg.cmplen = namelen;
@@ -273,20 +274,20 @@ sqfs_err sqfs_dir_lookup(sqfs *fs, sqfs_inode *inode,
 		if (order >= 0)
 			break;
 	}
-	
+
 	return err;
 }
 
 
 sqfs_err sqfs_lookup_path(sqfs *fs, sqfs_inode *inode, const char *path,
-		bool *found) {
+		bool *found, sqfs_inode_id *id) {
 	sqfs_err err;
 	sqfs_name buf;
 	sqfs_dir_entry entry;
 	
 	*found = false;
 	sqfs_dentry_init(&entry, buf);
-	
+
 	while (*path) {
 		const char *name;
 		size_t size;
@@ -307,7 +308,8 @@ sqfs_err sqfs_lookup_path(sqfs *fs, sqfs_inode *inode, const char *path,
 			return err;
 		if (!dfound)
 			return SQFS_OK; /* not found */
-		
+		if (id)
+			*id = sqfs_dentry_inode(&entry);
 		if ((err = sqfs_inode_get(fs, inode, sqfs_dentry_inode(&entry))))
 			return err;
 	}

dir.h
@@ -70,7 +70,7 @@ sqfs_err sqfs_dir_lookup(sqfs *fs, sqfs_inode *inode,
 /* Lookup a complete path, and replace *inode with the results.
 	 Uses / (slash) as the directory separator. */
 sqfs_err sqfs_lookup_path(sqfs *fs, sqfs_inode *inode, const char *path,
-	bool *found);
+	bool *found, sqfs_inode_id *id);
 
 
 /* Accessors on sqfs_dir_entry */

extract.c
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
     image = argv[1];
     path_to_extract = argv[2];
     
-    if ((err = sqfs_open_image(&fs, image, 0)))
+    if ((err = sqfs_open_image(&fs, image, 0, NULL)))
         exit(ERR_OPEN);
     
     if ((err = sqfs_traverse_open(&trv, &fs, sqfs_inode_root(&fs))))

fs.c
@@ -25,6 +25,7 @@
 #include "fs.h"
 
 #include "file.h"
+#include "dir.h"
 #include "nonstd.h"
 #include "swap.h"
 #include "xattr.h"
@@ -58,7 +59,7 @@ sqfs_compression_type sqfs_compression(sqfs *fs) {
 	return fs->sb.compression;
 }
 
-sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset) {
+sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir) {
 	sqfs_err err = SQFS_OK;
 	memset(fs, 0, sizeof(*fs));
 	
@@ -93,6 +94,20 @@ sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset) {
 	err |= sqfs_block_cache_init(&fs->data_cache, DATA_CACHED_BLKS);
 	err |= sqfs_block_cache_init(&fs->frag_cache, FRAG_CACHED_BLKS);
 	err |= sqfs_blockidx_init(&fs->blockidx);
+
+	if (subdir && subdir[0] != '\0') {
+		sqfs_inode root;
+		err |= sqfs_inode_get(fs, &root, sqfs_inode_root(fs));
+		sqfs_inode_id new_root;
+		bool found = false;
+		err |= sqfs_lookup_path(fs, &root, subdir, &found, &new_root);
+		if (!found) {
+			sqfs_destroy(fs);
+			return SQFS_ERR;
+		}
+		fs->sb.root_inode = new_root;
+	}
+
 	if (err) {
 		sqfs_destroy(fs);
 		return SQFS_ERR;

fs.h
@@ -88,7 +88,7 @@ void sqfs_version_supported(int *min_major, int *min_minor, int *max_major,
 size_t sqfs_divceil(uint64_t total, size_t group);
 
 
-sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset);
+sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir);
 void sqfs_destroy(sqfs *fs);
 
 /* Ok to call these even on incompletely constructed filesystems */

hl.c
@@ -51,7 +51,7 @@ static sqfs_err sqfs_hl_lookup(sqfs **fs, sqfs_inode *inode,
 		*inode = hl->root; /* copy */
 
 	if (path) {
-		sqfs_err err = sqfs_lookup_path(*fs, inode, path, &found);
+		sqfs_err err = sqfs_lookup_path(*fs, inode, path, &found, NULL);
 		if (err)
 			return err;
 		if (!found)
@@ -267,7 +267,7 @@ static int sqfs_hl_op_statfs(const char *path, struct statvfs *st) {
 }
 
 
-static sqfs_hl *sqfs_hl_open(const char *path, size_t offset) {
+static sqfs_hl *sqfs_hl_open(const char *path, size_t offset, const char *subdir) {
 	sqfs_hl *hl;
 	
 	hl = malloc(sizeof(*hl));
@@ -275,7 +275,7 @@ static sqfs_hl *sqfs_hl_open(const char *path, size_t offset) {
 		perror("Can't allocate memory");
 	} else {
 		memset(hl, 0, sizeof(*hl));
-		if (sqfs_open_image(&hl->fs, path, offset) == SQFS_OK) {
+		if (sqfs_open_image(&hl->fs, path, offset, subdir) == SQFS_OK) {
 			if (sqfs_inode_get(&hl->fs, &hl->root, sqfs_inode_root(&hl->fs)))
 				fprintf(stderr, "Can't find the root of this filesystem!\n");
 			else
@@ -296,6 +296,7 @@ int main(int argc, char *argv[]) {
 	
 	struct fuse_opt fuse_opts[] = {
 		{"offset=%zu", offsetof(sqfs_opts, offset), 0},
+		{"subdir=%s", offsetof(sqfs_opts, subdir), 0},
 		FUSE_OPT_END
 	};
 
@@ -322,6 +323,7 @@ int main(int argc, char *argv[]) {
 	
 	opts.progname = argv[0];
 	opts.image = NULL;
+	opts.subdir = NULL;
 	opts.mountpoint = 0;
 	opts.offset = 0;
 	if (fuse_opt_parse(&args, &opts, fuse_opts, sqfs_opt_proc) == -1)
@@ -329,7 +331,7 @@ int main(int argc, char *argv[]) {
 	if (!opts.image)
 		sqfs_usage(argv[0], true, false);
 	
-	hl = sqfs_hl_open(opts.image, opts.offset);
+	hl = sqfs_hl_open(opts.image, opts.offset, opts.subdir);
 	if (!hl)
 		return -1;
 	

ll.c
@@ -157,6 +157,7 @@ size_t sqfs_ll_add_direntry(fuse_req_t req, char *buf, size_t bufsize,
 		return esize;
 	#endif
 }
+
 void sqfs_ll_op_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
 		off_t off, struct fuse_file_info *fi) {
 	sqfs_err sqerr;
@@ -561,7 +562,7 @@ void teardown_idle_timeout() {
 	fuse_instance = NULL;
 }
 
-sqfs_ll *sqfs_ll_open(const char *path, size_t offset) {
+sqfs_ll *sqfs_ll_open(const char *path, size_t offset, const char *subdir) {
 	sqfs_ll *ll;
 	
 	ll = malloc(sizeof(*ll));
@@ -570,7 +571,7 @@ sqfs_ll *sqfs_ll_open(const char *path, size_t offset) {
 	} else {
 		memset(ll, 0, sizeof(*ll));
 		ll->fs.offset = offset;
-		if (sqfs_open_image(&ll->fs, path, offset) == SQFS_OK) {
+		if (sqfs_open_image(&ll->fs, path, offset, subdir) == SQFS_OK) {
 			if (sqfs_ll_init(ll))
 				fprintf(stderr, "Can't initialize this filesystem!\n");
 			else

ll.h
@@ -144,7 +144,7 @@ void setup_idle_timeout(struct fuse_session *se, unsigned int timeout_secs);
 
 void teardown_idle_timeout();
 
-sqfs_ll *sqfs_ll_open(const char *path, size_t offset);
+sqfs_ll *sqfs_ll_open(const char *path, size_t offset, const char *subdir);
 
 
 #endif

commit c5f60ed29ba5a6f1ed7d7233f795ee9141e01bf1

Restore functions signatures
subdir argument inroduced in 53e1b97 broke
multiple applications depending on squashfuse.
To fix this functions that accept sub_dir arg now have
_with_subdir suffix in them.
The old (unsuffixed) functions were reimplemented to call
their _with_subdir counterparts with subdir=NULL.

Fixes: #105

dir.c
@@ -279,7 +279,7 @@ sqfs_err sqfs_dir_lookup(sqfs *fs, sqfs_inode *inode,
 }
 
 
-sqfs_err sqfs_lookup_path(sqfs *fs, sqfs_inode *inode, const char *path,
+sqfs_err sqfs_lookup_path_with_id(sqfs *fs, sqfs_inode *inode, const char *path,
 		bool *found, sqfs_inode_id *id) {
 	sqfs_err err;
 	sqfs_name buf;
@@ -317,3 +317,8 @@ sqfs_err sqfs_lookup_path(sqfs *fs, sqfs_inode *inode, const char *path,
 	*found = true;
 	return SQFS_OK;
 }
+
+sqfs_err sqfs_lookup_path(sqfs *fs, sqfs_inode *inode, const char *path,
+		bool *found) {
+	return sqfs_lookup_path_with_id(fs, inode, path, found, NULL);
+};

dir.h
@@ -70,6 +70,11 @@ sqfs_err sqfs_dir_lookup(sqfs *fs, sqfs_inode *inode,
 /* Lookup a complete path, and replace *inode with the results.
 	 Uses / (slash) as the directory separator. */
 sqfs_err sqfs_lookup_path(sqfs *fs, sqfs_inode *inode, const char *path,
+	bool *found);
+
+/* Similar to sqfs_lookup_path, additionaly replaces *id
+   with sqfs_inode_id of inode */
+sqfs_err sqfs_lookup_path_with_id(sqfs *fs, sqfs_inode *inode, const char *path,
 	bool *found, sqfs_inode_id *id);
 
 

extract.c
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
     image = argv[1];
     path_to_extract = argv[2];
     
-    if ((err = sqfs_open_image(&fs, image, 0, NULL)))
+    if ((err = sqfs_open_image(&fs, image, 0)))
         exit(ERR_OPEN);
     
     if ((err = sqfs_traverse_open(&trv, &fs, sqfs_inode_root(&fs))))

fs.c
@@ -59,7 +59,7 @@ sqfs_compression_type sqfs_compression(sqfs *fs) {
 	return fs->sb.compression;
 }
 
-sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir) {
+sqfs_err sqfs_init_with_subdir(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir) {
 	sqfs_err err = SQFS_OK;
 	memset(fs, 0, sizeof(*fs));
 	
@@ -100,7 +100,7 @@ sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir) {
 		err |= sqfs_inode_get(fs, &root, sqfs_inode_root(fs));
 		sqfs_inode_id new_root;
 		bool found = false;
-		err |= sqfs_lookup_path(fs, &root, subdir, &found, &new_root);
+		err |= sqfs_lookup_path_with_id(fs, &root, subdir, &found, &new_root);
 		if (!found) {
 			sqfs_destroy(fs);
 			return SQFS_ERR;
@@ -114,6 +114,10 @@ sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir) {
 	}
 	
 	return SQFS_OK;
+}
+
+sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset) {
+  return sqfs_init_with_subdir(fs, fd, offset, NULL);
 }
 
 void sqfs_destroy(sqfs *fs) {

fs.h
@@ -88,7 +88,8 @@ void sqfs_version_supported(int *min_major, int *min_minor, int *max_major,
 size_t sqfs_divceil(uint64_t total, size_t group);
 
 
-sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir);
+sqfs_err sqfs_init(sqfs *fs, sqfs_fd_t fd, size_t offset);
+sqfs_err sqfs_init_with_subdir(sqfs *fs, sqfs_fd_t fd, size_t offset, const char *subdir);
 void sqfs_destroy(sqfs *fs);
 
 /* Ok to call these even on incompletely constructed filesystems */

hl.c
@@ -51,7 +51,7 @@ static sqfs_err sqfs_hl_lookup(sqfs **fs, sqfs_inode *inode,
 		*inode = hl->root; /* copy */
 
 	if (path) {
-		sqfs_err err = sqfs_lookup_path(*fs, inode, path, &found, NULL);
+		sqfs_err err = sqfs_lookup_path(*fs, inode, path, &found);
 		if (err)
 			return err;
 		if (!found)
@@ -275,7 +275,7 @@ static sqfs_hl *sqfs_hl_open(const char *path, size_t offset, const char *subdir
 		perror("Can't allocate memory");
 	} else {
 		memset(hl, 0, sizeof(*hl));
-		if (sqfs_open_image(&hl->fs, path, offset, subdir) == SQFS_OK) {
+		if (sqfs_open_image_with_subdir(&hl->fs, path, offset, subdir) == SQFS_OK) {
 			if (sqfs_inode_get(&hl->fs, &hl->root, sqfs_inode_root(&hl->fs)))
 				fprintf(stderr, "Can't find the root of this filesystem!\n");
 			else

ll.c
@@ -562,7 +562,7 @@ void teardown_idle_timeout() {
 	fuse_instance = NULL;
 }
 
-sqfs_ll *sqfs_ll_open(const char *path, size_t offset, const char *subdir) {
+sqfs_ll *sqfs_ll_open_with_subdir(const char *path, size_t offset, const char *subdir) {
 	sqfs_ll *ll;
 	
 	ll = malloc(sizeof(*ll));
@@ -571,7 +571,7 @@ sqfs_ll *sqfs_ll_open(const char *path, size_t offset, const char *subdir) {
 	} else {
 		memset(ll, 0, sizeof(*ll));
 		ll->fs.offset = offset;
-		if (sqfs_open_image(&ll->fs, path, offset, subdir) == SQFS_OK) {
+		if (sqfs_open_image_with_subdir(&ll->fs, path, offset, subdir) == SQFS_OK) {
 			if (sqfs_ll_init(ll))
 				fprintf(stderr, "Can't initialize this filesystem!\n");
 			else
@@ -582,4 +582,8 @@ sqfs_ll *sqfs_ll_open(const char *path, size_t offset, const char *subdir) {
 		free(ll);
 	}
 	return NULL;
+}
+
+sqfs_ll *sqfs_ll_open(const char *path, size_t offset) {
+	return sqfs_ll_open_with_subdir(path, offset, NULL);
 }

ll.h
@@ -144,7 +144,8 @@ void setup_idle_timeout(struct fuse_session *se, unsigned int timeout_secs);
 
 void teardown_idle_timeout();
 
-sqfs_ll *sqfs_ll_open(const char *path, size_t offset, const char *subdir);
+sqfs_ll *sqfs_ll_open_with_subdir(const char *path, size_t offset, const char *subdir);
+sqfs_ll *sqfs_ll_open(const char *path, size_t offset);
 
 
 #endif

ll_main.c
@@ -214,7 +214,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* OPEN FS */
-	err = !(ll = sqfs_ll_open(opts.image, opts.offset, opts.subdir));
+	err = !(ll = sqfs_ll_open_with_subdir(opts.image, opts.offset, opts.subdir));
 	
 	/* STARTUP FUSE */
 	if (!err) {

ls.c
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]) {
 		usage();
 	image = argv[1];
 
-	if ((err = sqfs_open_image(&fs, image, 0, NULL)))
+	if ((err = sqfs_open_image(&fs, image, 0)))
 		exit(ERR_OPEN);
 	
 	if ((err = sqfs_traverse_open(&trv, &fs, sqfs_inode_root(&fs))))

util.c
@@ -67,14 +67,14 @@
 
 /* TODO: WIN32 implementation of open/close */
 /* TODO: i18n of error messages */
-sqfs_err sqfs_open_image(sqfs *fs, const char *image, size_t offset, const char *subdir) {
+sqfs_err sqfs_open_image_with_subdir(sqfs *fs, const char *image, size_t offset, const char *subdir) {
 	sqfs_err err;
 	sqfs_fd_t fd;
 
 	if ((err = sqfs_fd_open(image, &fd, stderr)))
 		return err;
 
-	err = sqfs_init(fs, fd, offset, subdir);
+	err = sqfs_init_with_subdir(fs, fd, offset, subdir);
 	switch (err) {
 		case SQFS_OK:
 			break;
@@ -123,3 +123,6 @@ sqfs_err sqfs_open_image(sqfs *fs, const char *image, size_t offset, const char 
 	return err;
 }
 
+sqfs_err sqfs_open_image(sqfs *fs, const char *image, size_t offset) {
+	return sqfs_open_image_with_subdir(fs, image, offset, NULL);
+}

util.h
@@ -35,7 +35,10 @@ sqfs_err sqfs_fd_open(const char *path, sqfs_fd_t *fd, bool print);
 /* Close a file */
 void sqfs_fd_close(sqfs_fd_t fd);
 
+/* Open a filesystem with subdir and print errors to stderr. */
+sqfs_err sqfs_open_image_with_subdir(sqfs *fs, const char *image, size_t offset, const char *subdir);
+
 /* Open a filesystem and print errors to stderr. */
-sqfs_err sqfs_open_image(sqfs *fs, const char *image, size_t offset, const char *subdir);
+sqfs_err sqfs_open_image(sqfs *fs, const char *image, size_t offset);
 
 #endif