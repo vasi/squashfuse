b44be7ec8e6a01e74c7533e8b85fe558e89ccb0e
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2023-11-21 05:22:32 -0700

    Create shell.nix (#122)
    * Create shell.nix
    
    This functions as an easy way to set up a development environment for anyone using the Nix package manager
    
    * Add NixOS to PLATFORMS
    
    * Add NixOS build instructions
    
    * Update shell.nix
    
    Add gnumake and fuse 2
    
    gnumake comes default on NixOS but may not on other Linux systems using the package manager

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    # Required build tools
+    gcc
+    gnumake
+    autoconf
+    automake
+    libtool
+    pkg-config
+
+    # Required libraries
+    fuse
+    fuse3
+    zlib
+    zstd
+    lzo
+    lz4
+    xz
+  ];
+}
